84cbaa898defee1b1178f4b129b18269254ca5e6
sabaaassi <90206834+sabaaassi@users.noreply.github.com>
2024-07-02 23:06:23 +0300

    to update the Data

--- js/LivingRoom.js
+++ js/LivingRoom.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch('data/data.json')
+    fetch('Data/data.json')
       .then(response => response.json())
       .then(data => {
         devicesData = data.devices; 
